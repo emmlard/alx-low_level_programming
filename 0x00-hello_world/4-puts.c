97732b12ef4a5f9c610366469aebbe2e7095f9b0
emmlard <emmlard@gmail.com>
2022-06-02 20:05:30 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+*  main - print to the standard output 'Programming is multilingual puzzle'
+*
+*  Use Puts
+*  Return: retrn 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
