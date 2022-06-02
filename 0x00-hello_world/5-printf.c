22664475849dd905dad545af146343383862c707
emmlard <emmlard@gmail.com>
2022-06-02 20:53:31 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*  main - print to the standard output 'Programming is multilingual puzzle'
+*
+*  Use Printf
+*  Return: retrn 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
