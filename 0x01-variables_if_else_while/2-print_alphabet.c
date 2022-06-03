f384f3a74763d3fa71cc255f00b3f797a3bc73f7
emmlard <emmlard@gmail.com>
2022-06-03 20:36:33 +0100

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print to the standard output 'alphabet in lowercase'
+*
+* Return: return 0
+*/
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	return (0);
+}
