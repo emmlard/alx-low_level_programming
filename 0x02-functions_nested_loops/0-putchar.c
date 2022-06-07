309a965b55c1583e829a0f17ec54170194b32b04
emmlard <emmlard@gmail.com>
2022-06-07 15:08:02 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,15 @@
 
 int main(void)
 {
-	char a = "_putchar";
+	char school[10] = "_putchar";
+	int a;
 
-	putchar("%c \n", a);
+	for (a = 0; a <= 7; a++)
+	{
+	_putchar(school[a]);
+
+	}
+	_putchar(9);
+
 	return (0);
 }
