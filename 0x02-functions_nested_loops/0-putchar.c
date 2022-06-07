4f431cf5bc35109de17688d3530a9fc41b465405
emmlard <emmlard@gmail.com>
2022-06-07 14:01:43 +0100

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
+#include "main.h"
+
 /**
-* 
+* main - Write a program that prints _putchar
 *
+* Return: return 0
 */
 
-int 
+int main(void)
+{
+	char a = "_putchar";
+
+	putchar("%c \n", a);
+	return (0);
+}
