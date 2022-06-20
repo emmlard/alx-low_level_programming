a8a47925ac2ae0850b3985d04fa94d3ba5280f1c
emmlard <emmlard@gmail.com>
2022-06-20 22:33:42 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strchr - locates a character in a string
+* @s: string
+* @c: first character
+*
+* Return: pointer to the first occurrence of the character c or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	s = &c;
+	
+	if (*(s + 0) != '\0')
+		return (s);
+	else
+		return ('\0');
+}
