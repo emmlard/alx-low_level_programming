5aa44856b763236bbbd0fbaa01490981da24ad9c
emmlard <emmlard@gmail.com>
2022-06-20 22:16:58 +0100

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memcpy - Copy memory area
+* @dest: memory area destination
+* @src: memory area from
+* @n: byte of memory area
+*
+* Return: pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@
 #ifndef main_h
 
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
